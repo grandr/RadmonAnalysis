5016cdcf808d9a7c2f774d34d0db022e41ae1604
Andrei Gribushin <grandr@cern.ch>
2016-07-12 15:28:54 +0300

    cleanup

--- ToOnlineLumi/mergeWithLumi.py
+++ ToOnlineLumi/mergeWithLumi.py
@@ -5,6 +5,8 @@
 Merge RadmonData & LumiData based on run/ls
 """
 import sys, os
+sys.path.append("../Utils/")
+sys.path.append("../Config/")
 from array import array
 from ROOT import *
 import math
@@ -13,15 +15,24 @@ from xutils import *
 from fillReport import *
 
 #fills = [3976]
-#
-fills = [ 3960, 3962, 3965, 3971, 3974,3976, 3981, 3983, 3986, 3988, 3992, 3996, 4001, 4006, 4008, 4019, 4020, 4201, 4205, 4207, 4208, 4210, 4211, 4212, 4214, 4219, 4220, 4224, 4225, 4231, 4243, 4246, 4249, 4254, 4256, 4257, 4266, 4268, 4269, 4322, 4323, 4332, 4337, 4341, 4342, 4349, 4356, 4360, 4363, 4364, 4376, 4381, 4384, 4386, 4391, 4393, 4397, 4398, 4402, 4410, 4418, 4420, 4423, 4426, 4428, 4432, 4434, 4435, 4437, 4440, 4444, 4448, 4449, 4452, 4455, 4462, 4463, 4464, 4466, 4467,4476, 4477, 4479, 4485, 4495, 4496, 4499, 4505, 4509, 4510, 4511, 4513, 4518, 4519, 4522, 4525, 4528, 4530, 4532, 4536, 4538, 4540, 4545, 4555, 4557, 4560, 4562, 4565, 4569]
+#  2015
+#fills = [ 3960, 3962, 3965, 3971, 3974,3976, 3981, 3983, 3986, 3988, 3992, 3996, 4001, 4006, 4008, 4019, 4020, 4201, 4205, 4207, 4208, 4210, 4211, 4212, 4214, 4219, 4220, 4224, 4225, 4231, 4243, 4246, 4249, 4254, 4256, 4257, 4266, 4268, 4269, 4322, 4323, 4332, 4337, 4341, 4342, 4349, 4356, 4360, 4363, 4364, 4376, 4381, 4384, 4386, 4391, 4393, 4397, 4398, 4402, 4410, 4418, 4420, 4423, 4426, 4428, 4432, 4434, 4435, 4437, 4440, 4444, 4448, 4449, 4452, 4455, 4462, 4463, 4464, 4466, 4467,4476, 4477, 4479, 4485, 4495, 4496, 4499, 4505, 4509, 4510, 4511, 4513, 4518, 4519, 4522, 4525, 4528, 4530, 4532, 4536, 4538, 4540, 4545, 4555, 4557, 4560, 4562, 4565, 4569]
+#lumiFilePattern = '/home/data/OnlineLumidata/2015/lumi__XXX__.root'
+#radmonLumiFilePattern = '/home/data/RadmonLumi/2015/radmonLumi__XXX__.root'
+#fillReportName = '/home/grandr/cms/Bril/Analysis/ToOnlineLumi/FillReport_1446656923991.xls'
 
-lumiFilePattern = '/home/data/OnlineLumidata/lumi__XXX__.root'
+#2016
+fills =  [4888, 4889, 4890, 4892, 4895, 4896, 4905, 4906, 4910, 4915, 4919, 4924, 4925, 4926, 4930, 4935, 4937, 4942, 4945, 4947, 4953, 4954, 4956, 4958, 4960, 4961, 4964, 4965, 4976, 4979, 4980, 4984, 4985, 4988, 4990, 5005, 5013, 5017, 5020, 5021, 5024, 5026, 5027, 5028, 5029, 5030, 5038, 5043, 5045, 5048, 5052]
+lumiFilePattern = '/home/data/OnlineLumidata/2016/lumi__XXX__.root'
+radmonLumiFilePattern = '/home/data/RadmonLumi/2016/radmonLumi__XXX__.root'
+fillReportName = '../Config/FillReport.xls'
+
+
 RadmonDataDir = '/home/data/RadMonData/RawData'
 radmonFilePattern = 'HFRadmonData*.root'
-radmonLumiFilePattern = '/home/data/RadmonLumi/radmonLumi__XXX__.root'
 
-fillReportName = '/home/grandr/cms/Bril/Analysis/ToOnlineLumi/FillReport_1446656923991.xls'
+
+
 
 # temporary fix (rate higher than this value means that detector is being turned on)
 rateMax = 100000. 
